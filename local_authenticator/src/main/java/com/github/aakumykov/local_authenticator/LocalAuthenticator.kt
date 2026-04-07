c51f91490d4686a7159d9515395c6910a136fa67
Andrey Kumykov <aakumykov@yandex.ru>
2026-04-07 20:29:44 +0300

    Прикрутить StorageAccessHelper со своей скрытой сложной логикой не так-то просто.

--- local_authenticator/src/main/java/com/github/aakumykov/local_authenticator/LocalAuthenticator.kt
+++ local_authenticator/src/main/java/com/github/aakumykov/local_authenticator/LocalAuthenticator.kt
@@ -1,17 +1,42 @@
 package com.github.aakumykov.local_authenticator
 
 import android.content.Intent
+import androidx.activity.ComponentActivity
 import androidx.activity.result.ActivityResultLauncher
+import androidx.fragment.app.Fragment
 import com.github.aakumykov.cloud_authenticator.CloudAuthenticator
+import com.github.aakumykov.storage_access_helper.StorageAccessHelper
 
-class LocalAuthenticator(private val cloudAuthenticatorCallbacks: Callbacks) : CloudAuthenticator() {
+class LocalAuthenticator(
+    private val cloudAuthenticatorCallbacks: Callbacks
+) : CloudAuthenticator() {
+
+    private var _storageAccessHelper: StorageAccessHelper? = null
+    private val storageAccessHelper: StorageAccessHelper get() = _storageAccessHelper!!
+
+    override fun prepareResultLauncher(componentActivity: ComponentActivity): ActivityResultLauncher<Intent> {
+        _storageAccessHelper = StorageAccessHelper.create(componentActivity).apply {
+            prepareForFullAccess()
+        }
+        return super.prepareResultLauncher(componentActivity)
+    }
+
+    override fun prepareResultLauncher(fragment: Fragment): ActivityResultLauncher<Intent> {
+        _storageAccessHelper = StorageAccessHelper.create(fragment).apply {
+            prepareForFullAccess()
+        }
+        return super.prepareResultLauncher(fragment)
+    }
 
     override fun startAuth(activityResultLauncher: ActivityResultLauncher<Intent>) {
-        cloudAuthenticatorCallbacks.onCloudAuthSuccess(DUMMY_AUTH_TOKEN)
+        storageAccessHelper.requestFullAccess { isGranted ->
+            if (isGranted) cloudAuthenticatorCallbacks.onCloudAuthSuccess(DUMMY_AUTH_TOKEN)
+            else cloudAuthenticatorCallbacks.onCloudAuthFailed(Exception("Access to storage denied."))
+        }
     }
 
     override fun processAuthResult(resultCode: Int, data: Intent?) {
-        cloudAuthenticatorCallbacks.onCloudAuthSuccess(DUMMY_AUTH_TOKEN)
+
     }
 
     override fun deAuth() {
