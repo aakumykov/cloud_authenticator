8989444157f179eb39f7a365c3131a6513fc62cd
Andrey Kumykov <aakumykov@yandex.ru>
2026-04-07 20:32:26 +0300

    Revert "Прикрутить StorageAccessHelper со своей скрытой сложной логикой не так-то просто."
    This reverts commit c51f91490d4686a7159d9515395c6910a136fa67.

--- cloud_authenticator/src/main/java/com/github/aakumykov/cloud_authenticator/CloudAuthenticator.kt
+++ cloud_authenticator/src/main/java/com/github/aakumykov/cloud_authenticator/CloudAuthenticator.kt
@@ -1,24 +1,9 @@
 package com.github.aakumykov.cloud_authenticator
 
 import android.content.Intent
-import androidx.activity.ComponentActivity
 import androidx.activity.result.ActivityResultLauncher
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.fragment.app.Fragment
 
 abstract class CloudAuthenticator() {
-
-    open fun prepareResultLauncher(componentActivity: ComponentActivity): ActivityResultLauncher<Intent> {
-        return componentActivity.registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-            processAuthResult(result.resultCode, result.data)
-        }
-    }
-
-    open fun prepareResultLauncher(fragment: Fragment): ActivityResultLauncher<Intent> {
-        return fragment.registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-            processAuthResult(result.resultCode, result.data)
-        }
-    }
 
     abstract fun startAuth(activityResultLauncher: ActivityResultLauncher<Intent>)
     abstract fun processAuthResult(resultCode: Int, data: Intent?)
